b3db116cc408f88d1ac5f27db942d0b97d5ef99f
Faizan <developingfaizaan@gmail.com>
2022-11-30 18:43:40 +0530

    setting headers for cors issue

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const morgan = require("morgan");
 const helmet = require("helmet");
-const cors = require("cors");
+// const cors = require("cors");
 const mongoose = require("mongoose");
 
 const { notFound, errorHandler } = require("./middlewares/errorHandler");
@@ -17,13 +17,28 @@ app.use(morgan("dev"));
 app.use(express.json({limit: '50mb'}));
 app.use(express.urlencoded({limit: '50mb'}));
 app.use(express.json());
+// app.use(cors({ origin: "https://pflegebook.net/"}));
+// ,"https://caregiver-platform.vercel.app/","http://localhost:3000"]
 
-const corsOptions = {
-  origin: ["https://pflegebook.net/","https://caregiver-platform.vercel.app/","http://localhost:3000"],
-  optionsSuccessStatus: 200 // For legacy browser support
-} 
-app.use(cors(corsOptions));
+// Add headers before the routes are defined
+app.use(function (req, res, next) {
 
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', 'https://pflegebook.net');
+
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
+  // Pass to next layer of middleware
+  next();
+});
 
 app.get("/", (req, res) => {
   res.json({
